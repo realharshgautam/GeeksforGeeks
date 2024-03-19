ed66611b00e30491b0c217721dabbcea6c3f018d
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2024-03-19 07:51:22 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Bitonic Point/bitonic-point.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	int findMaximum(int arr[], int n) {
+	   sort(arr,arr+n);
+	   
+	    return arr[n-1];
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMaximum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
