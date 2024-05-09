c8bd086489b0ce14bf3aed331969982f04d6c94e
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2024-05-09 14:41:28 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Data Type/data-type.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+// {Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int dataTypeSize(string str) {
+        if(str == "Character")  return sizeof(char);
+        else if(str == "Integer")  return sizeof(int);
+        else if(str == "Long")  return sizeof(long);
+        else if(str == "Float")  return sizeof(float);
+        return sizeof(double);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.dataTypeSize(str);
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
