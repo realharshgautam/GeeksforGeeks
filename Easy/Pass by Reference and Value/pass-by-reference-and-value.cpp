df5e31139ad3bb429c12d3d320e9f9e1cb8d5c97
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2024-05-09 14:49:47 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pass by Reference and Value/pass-by-reference-and-value.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> passedBy(int a, int &b) {
+        vector<int>res={a+1,b+2};
+        return  res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        Solution obj;
+        vector<int> ans = obj.passedBy(a, b);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
